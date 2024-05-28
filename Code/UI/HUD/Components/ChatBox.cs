3958e41b45eb67e03095bbfc73675fe5d34a4781
Alex <12881812+xezno@users.noreply.github.com>
2024-05-28 16:30:50 +0100

    Chat fixes + team chat

---
+++ Code/UI/HUD/Components/ChatBox.cs
@@ -0,0 +1,21 @@
+﻿namespace Facepunch.UI;
+
+internal class ChatBox : TextEntry
+{
+	public Action OnTabPressed { get; set; }
+
+	public override void OnButtonTyped(ButtonEvent e)
+	{
+		e.StopPropagation = true;
+
+		var button = e.Button;
+
+		if (button == "tab")
+		{
+			Log.Info("Tab");
+			OnTabPressed?.Invoke();
+		}
+
+		base.OnButtonTyped(e);
+	}
+}
