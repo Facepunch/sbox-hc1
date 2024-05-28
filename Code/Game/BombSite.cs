f117a96091dad85e75bea4c029ae1ff7298ca08f
James King <james.king@facepunchstudios.com>
2024-05-28 18:13:24 +0200

    BombSite component, update BombSite_A.prefab

---
+++ Code/Game/BombSite.cs
@@ -0,0 +1,24 @@
+﻿
+/// <summary>
+/// C4 can be planted anywhere within colliders inside this object.
+/// </summary>
+public sealed class BombSite : Component
+{
+	/// <summary>
+	/// Which bomb site is this (A: 0, B: 1, maybe more!).
+	/// </summary>
+	[Property]
+	public int Index { get; set; }
+
+	protected override void DrawGizmos()
+	{
+		Gizmo.Draw.Color = Gizmo.Colors.Red.WithAlpha( Gizmo.IsSelected ? 0.5f : 0.25f );
+
+		foreach ( var collider in Components.GetAll<BoxCollider>() )
+		{
+			Gizmo.Transform = collider.Transform.World;
+
+			Gizmo.Draw.SolidBox( BBox.FromPositionAndSize( collider.Center, collider.Scale ) );
+		}
+	}
+}
