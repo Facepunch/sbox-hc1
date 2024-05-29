d771d87e921f004ff0351b610fffaf43b47c519e
Alex <12881812+xezno@users.noreply.github.com>
2024-05-29 21:42:44 +0100

    Experience -> Score, add some cases

--- Code/Player/PlayerScore.cs
+++ Code/Player/PlayerScore.cs
@@ -9,10 +9,30 @@ public sealed class PlayerScore : Component, IKillListener
 	public int Deaths { get; set; } = 0;
 
 	[HostSync, Property, ReadOnly] 
-	public int Experience { get; set; } = 0;
+	public int Score { get; set; } = 0;
 
-	private const int FriendlyKillScore = -200;
-	private const int KillScore = 100;
+	private const int KillScore = 2;
+	private const int AssistScore = 1;
+	private const int TeamKillScore = -1;
+	private const int SuicideScore = -1;
+
+	// Planting the C4 explosive
+	private const int PlantScore = 2;
+
+	// Bomb planter alive when the bomb explodes
+	private const int BombExplodePlanterAliveScore = 2;
+
+	// Bomb planter dead when the bomb explodes
+	private const int BombExplodePlanterDeadScore = 1;
+
+	// Other Ts alive when the bomb explodes
+	private const int BombExplodeTeamAliveScore = 1;
+
+	// Defusing bomb
+	private const int DefuserScore = 2;
+
+	// Other CTs alive when the bomb is defused
+	private const int DefuseTeamAliveScore = 1;
 
 	public void OnPlayerKilled( Component killer, Component victim, float damage, Vector3 position, Vector3 force, Component inflictor = null )
 	{
@@ -25,23 +45,31 @@ public sealed class PlayerScore : Component, IKillListener
 		bool isFriendly = killerPlayer.TeamComponent.Team == victimPlayer.TeamComponent.Team;
 		bool isSuicide = killerPlayer == victimPlayer;
 
-		if ( isFriendly || isSuicide )
+		if ( killerPlayer == thisPlayer )
 		{
-			if ( killerPlayer == thisPlayer )
+			if ( isFriendly )
 			{
+				// Killed by friendly/teammate
 				Kills--;
-				Experience += FriendlyKillScore;
+				Score += TeamKillScore;
+			}
+			else if ( isSuicide )
+			{
+				// Killed by suicide
+				Kills--;
+				Score += SuicideScore;
+			}
+			else
+			{
+				// Valid kill, add score
+				Kills++;
+				Score += KillScore;
 			}
 		}
-		else
+		else if ( victimPlayer == thisPlayer )
 		{
-			// Valid kill, add score
-			if ( killerPlayer == thisPlayer )
-			{
-				Kills++;
-				Experience += KillScore;
-			}
-			else if ( victimPlayer == thisPlayer )
+			// Only count as death if this wasn't a team kill
+			if ( !isFriendly )
 			{
 				Deaths++;
 			}
