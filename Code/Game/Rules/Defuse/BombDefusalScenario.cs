80ccd596da7f439d38d8d9ba01375b6db8b8a948
James King <james.king@facepunchstudios.com>
2024-05-29 17:08:54 +0200

    Some team income values

--- Code/Game/Rules/Defuse/BombDefusalScenario.cs
+++ Code/Game/Rules/Defuse/BombDefusalScenario.cs
@@ -13,6 +13,32 @@ public sealed class BombDefusalScenario : Component,
 	[RequireComponent] public TeamEliminated TeamEliminated { get; private set; }
 	[RequireComponent] public TeamScoring TeamScoring { get; private set; }
 
+	[Property, HostSync, Category( "Economy" )]
+	public int StartMoney { get; set; } = 800;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int DefaultWinTeamIncome { get; set; } = 3250;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int BombDefusedTeamIncome { get; set; } = 3500;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int BombDetonatedTeamIncome { get; set; } = 3500;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int BaseLossTeamIncome { get; set; } = 1400;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int LossStreakBonus { get; set; } = 500;
+
+	[Property, HostSync, Category( "Economy" )]
+	public int MaxLossStreakBonus { get; set; } = 2000;
+
+	public int GetLossStreakBonus( int roundsLost )
+	{
+		return BaseLossTeamIncome + Math.Min( (roundsLost - 1) * LossStreakBonus, MaxLossStreakBonus );
+	}
+
 	[HostSync] public bool IsBombPlanted { get; private set; }
 	[HostSync] public bool BombHasDetonated { get; private set; }
 	[HostSync] public bool BombWasDefused { get; private set; }
