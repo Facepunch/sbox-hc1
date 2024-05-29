5cf639685c1f99451c7add0c51b1260a393aed9c
James King <james.king@facepunchstudios.com>
2024-05-29 18:51:39 +0200

    NetDictionary.GetValueOrDefault() extension

---
+++ Code/Utils/CollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Facepunch;
+
+public static class CollectionExtensions
+{
+	public static TValue GetValueOrDefault<TKey, TValue>( this NetDictionary<TKey, TValue> dict, TKey key, TValue defaultValue = default )
+	{
+		return dict.TryGetValue( key, out var value ) ? value : defaultValue;
+	}
+}
+
