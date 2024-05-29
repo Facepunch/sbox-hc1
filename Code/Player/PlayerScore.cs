810ca3f014377d55f3f70f823d2a6a001af31383
Alex <12881812+xezno@users.noreply.github.com>
2024-05-29 14:23:01 +0100

    Basic scoreboard, PlayerScore component

---
+++ Code/Player/PlayerScore.cs
@@ -0,0 +1,8 @@
+﻿namespace Facepunch.UI;
+
+public sealed class PlayerScore : Component
+{
+	[Sync] public int Kills { get; set; } = 0;
+	[Sync] public int Deaths { get; set; } = 0;
+	[Sync] public int Experience { get; set; } = 0;
+}
