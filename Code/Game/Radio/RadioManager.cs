f348496b890d7f140b754b9200afb62bbf592eeb
Tony Ferguson <devultj@gmail.com>
2024-05-29 15:42:39 +0100

    Integrate bomb planted, bomb defused, round won, round loss radio sounds

--- Code/Game/Radio/RadioManager.cs
+++ Code/Game/Radio/RadioManager.cs
@@ -3,7 +3,7 @@ namespace Facepunch;
 /// <summary>
 /// Handles certain events and plays radio sounds.
 /// </summary>
-public partial class RadioManager : Component, IRoundStartListener
+public partial class RadioManager : Component, IRoundStartListener, IBombPlantedListener, IBombDefusedListener
 {
 	public static RadioManager Instance { get; private set; }
 
@@ -16,5 +16,17 @@ public partial class RadioManager : Component, IRoundStartListener
 	{
 		RadioSounds.Play( Team.Terrorist, RadioSound.RoundStarted );
 		RadioSounds.Play( Team.CounterTerrorist, RadioSound.RoundStarted );
+	}
+
+	void IBombPlantedListener.OnBombPlanted( PlayerController planter, GameObject bomb, BombSite bombSite )
+	{
+		RadioSounds.Play( Team.Terrorist, RadioSound.BombPlanted );
+		RadioSounds.Play( Team.CounterTerrorist, RadioSound.BombPlanted );
+	}
+
+	void IBombDefusedListener.OnBombDefused( PlayerController planter, GameObject bomb, BombSite bombSite )
+	{
+		RadioSounds.Play( Team.Terrorist, RadioSound.BombDefused );
+		RadioSounds.Play( Team.CounterTerrorist, RadioSound.BombDefused );
 	}
 }
