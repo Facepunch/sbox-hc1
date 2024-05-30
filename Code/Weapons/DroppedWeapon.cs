318bd30d68ae8f6dfae75be0808b1571236c80d1
Tony Ferguson <devultj@gmail.com>
2024-05-30 09:51:20 +0100

    Don't collide with dropped weapons, overlapping with weapon will try to pick it up

--- Code/Weapons/DroppedWeapon.cs
+++ Code/Weapons/DroppedWeapon.cs
@@ -1,6 +1,6 @@
 namespace Facepunch;
 
-public partial class DroppedWeapon : Component, IUse
+public partial class DroppedWeapon : Component, IUse, Component.ICollisionListener
 {
 	[Property] public WeaponData Resource { get; set; }
 
@@ -9,6 +9,7 @@ public partial class DroppedWeapon : Component, IUse
 	public static DroppedWeapon Create( WeaponData resource, Vector3 positon, Rotation rotation = default )
 	{
 		var go = new GameObject();
+		go.Tags.Set( "no_player", true );
 		go.Transform.Position = positon;
 		go.Transform.Rotation = rotation;
 		go.Name = resource.Name;
@@ -34,9 +35,25 @@ public partial class DroppedWeapon : Component, IUse
 		return player.Inventory.CanTakeWeapon( Resource );
 	}
 
+	private bool _isUsed;
+
 	public void OnUse( PlayerController player )
 	{
+		if ( _isUsed ) return;
+		_isUsed = true;
+
 		player.Inventory.GiveWeapon( Resource );
 		GameObject.Destroy();
+	}
+
+	void ICollisionListener.OnCollisionStart( Collision collision )
+	{
+		if ( collision.Other.GameObject.Root.Components.Get<PlayerController>( FindMode.EnabledInSelfAndDescendants ) is { } player )
+		{
+			if ( player.Inventory.CanTakeWeapon( Resource ) )
+			{
+				OnUse( player );
+			}
+		}
 	}
 }
