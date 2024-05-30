517f085bcede2d6306e4774b47a8e047f9985f97
kurozael <kurozael@gmail.com>
2024-05-30 17:25:18 +0100

    Update Outfit if team changes

--- Code/Player/PlayerOutfitter.cs
+++ Code/Player/PlayerOutfitter.cs
@@ -7,15 +7,37 @@ public partial class PlayerOutfitter : Component
 {
 	[Property] public GameObject TerroristClothes { get; set; }
 	[Property] public GameObject CounterTerroristClothes { get; set; }
+	[Property] public TeamComponent TeamComponent { get; set; }
+	[Property] public PlayerController PlayerController { get; set; }
 
 	public void OnResetState( PlayerController player )
 	{
 		UpdateFromTeam( player.TeamComponent.Team );
 	}
 
+	private void OnTeamChanged( Team oldTeam, Team newTeam )
+	{
+		if ( PlayerController.IsSpectating )
+			return;
+		
+		UpdateFromTeam( newTeam );
+	}
+
+	protected override void OnEnabled()
+	{
+		TeamComponent.OnTeamChanged += OnTeamChanged;
+		base.OnEnabled();
+	}
+
+	protected override void OnDisabled()
+	{
+		TeamComponent.OnTeamChanged -= OnTeamChanged;
+		base.OnDisabled();
+	}
+
 	private void UpdateFromTeam( Team team )
 	{
-		bool isT = team == Team.Terrorist;
+		var isT = team == Team.Terrorist;
 		TerroristClothes.Enabled = isT;
 		CounterTerroristClothes.Enabled = !isT;
 	}
