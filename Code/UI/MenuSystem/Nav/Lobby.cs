253e4d611492fa480dd5c8ec48761645add88d0d
Tony Ferguson <devultj@gmail.com>
2024-05-31 23:23:41 +0100

    First main menu iteration, can display lobbies and join them, or create them

---
+++ Code/UI/MenuSystem/Nav/Lobby.cs
@@ -0,0 +1,27 @@
+using Sandbox.Network;
+
+namespace Facepunch;
+
+/// <summary>
+/// This purely exists as a wrappe for a real lobby, so we can display fake lobbies in the UI.
+/// </summary>
+public partial struct Lobby
+{
+	/// <summary>
+	/// A reference to our internal lobby type.
+	/// </summary>
+	public LobbyInformation? _lobby;
+
+	public string Name => _lobby?.Name ?? "My cool lobby";
+	public int Members => _lobby?.Members ?? 8;
+	public int MaxMembers => _lobby?.MaxMembers ?? 8;
+	public string Map => _lobby?.Map;
+	public ulong LobbyId => _lobby?.LobbyId ?? 0;
+
+	public Lobby( LobbyInformation lobby )
+	{
+		_lobby = lobby;
+	}
+
+	public Lobby() { }
+}
