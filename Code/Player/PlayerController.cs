1827a8e1340ada7450aadf6a38ebd5e0878cc3f4
Sol Williams <7689429+solwllms@users.noreply.github.com>
2024-05-31 10:46:44 +0100

    Fix GetPlayerName exception

--- Code/Player/PlayerController.cs
+++ Code/Player/PlayerController.cs
@@ -154,7 +154,7 @@ public partial class PlayerController : Component, IPawn, IRespawnable, IDamageL
 	/// </summary>
 	public bool IsBot => BotId != -1;
 
-	public string GetPlayerName() => IsBot ? $"BOT {BotManager.Instance.GetName(BotId)}" : Network.OwnerConnection.DisplayName;
+	public string GetPlayerName() => IsBot ? $"BOT {BotManager.Instance.GetName(BotId)}" : Network.OwnerConnection?.DisplayName ?? "";
 
 	/// <summary>
 	/// If true, we're not allowed to move.
