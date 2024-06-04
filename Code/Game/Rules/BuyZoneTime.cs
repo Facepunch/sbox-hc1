8ba46908366dcd963a74c4aa2f13f45b4e7b9655
Tony Ferguson <devultj@gmail.com>
2024-06-04 14:57:59 +0100

    Fixed not being able to buy at game start, pre-round start

--- Code/Game/Rules/BuyZoneTime.cs
+++ Code/Game/Rules/BuyZoneTime.cs
@@ -5,9 +5,14 @@ namespace Facepunch;
 public sealed class BuyZoneTime : Component, IRoundStartListener
 {
 	[Property] public float BuyTime { get; set; } = 30;
-	[HostSync] public RealTimeUntil TimeUntilCannotBuy { get; private set; } = 0;
+	[HostSync] public RealTimeUntil TimeUntilCannotBuy { get; private set; } = 60;
 
 	void IRoundStartListener.PostRoundStart()
+	{
+		TimeUntilCannotBuy = BuyTime;
+	}
+
+	void IRoundStartListener.PreRoundStart()
 	{
 		TimeUntilCannotBuy = BuyTime;
 	}
