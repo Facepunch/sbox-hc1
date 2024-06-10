7b70e6cf82ec8f482b45f5d4e14bec0878fe2e5d
bakscratch <lt_8@hotmail.co.uk>
2024-06-10 11:37:33 +0100

    Hide Object command

---
+++ Editor/HideObject.cs
@@ -0,0 +1,21 @@
+﻿using Editor;
+using Sandbox;
+
+public class HideObject
+{
+	[Menu("Editor", "Settings/HC1/ToggleSelection", Shortcut = "h" )]
+	static void ToggleSelection()
+	{
+		using var scope = SceneEditorSession.Scope();
+
+		var selection = EditorScene.Selection;
+
+		foreach ( var item in EditorScene.Selection )
+		{
+			var thing = item as GameObject;
+
+			thing.Enabled = !thing.Enabled;
+		}
+
+	}
+}
