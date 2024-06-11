c0a247b5d5d5313151d15a0334b5f5002d4a507c
Tony Ferguson <devultj@gmail.com>
2024-06-11 14:40:24 +0100

    Voice chat filter sends to spectators

--- Code/GameLoop/Rules/TeamVoiceChatFilter.cs
+++ Code/GameLoop/Rules/TeamVoiceChatFilter.cs
@@ -2,10 +2,19 @@ namespace Facepunch;
 
 public partial class TeamVoiceChatFilter : Component, IVoiceFilter
 {
+	private bool DontSendFilter( PlayerController player )
+	{
+		// Send voices to spectators
+		if ( player.TeamComponent.Team == Team.Unassigned ) return false;
+
+		// Don't send to enemies
+		return !player.IsFriendly( GameUtils.LocalPlayer );
+	}
+
 	IEnumerable<Connection> IVoiceFilter.GetExcludeFilter()
 	{
 		return Scene.GetAllComponents<PlayerController>()
-			.Where( x => !x.IsFriendly( GameUtils.LocalPlayer ) )
+			.Where( DontSendFilter )
 			.Select( x => x.Network.OwnerConnection );
 	}
 }
