48273395e1da322800bd3ead3a613929b5f2965e
Tony Ferguson <devultj@gmail.com>
2024-06-11 16:40:57 +0100

    Added ADS

---
+++ Code/Weapons/Components/AimWeaponComponent.cs
@@ -0,0 +1,34 @@
+namespace Facepunch;
+
+public partial class AimWeaponFunction : InputWeaponComponent
+{
+	[Sync] public bool IsAiming { get; set; }
+
+	[Property] public Vector3 AimOffset { get; set; }
+	[Property] public Angles AimAngles { get; set; }
+
+	protected override void OnEnabled()
+	{
+		BindTag( "aiming", () => IsAiming );
+	}
+
+	protected virtual bool CanAim()
+	{
+		if ( Tags.Has( "no_aiming" ) ) return false;
+		if ( Tags.Has( "reloading" ) ) return false;
+		if ( Weapon.PlayerController.Tags.Has( "no_aiming" ) ) return false;
+
+		return true;
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( !CanAim() )
+		{
+			IsAiming = false;
+			return;
+		}
+
+		IsAiming = IsDown();
+	}
+}
