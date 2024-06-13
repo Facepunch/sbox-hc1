c44c9fcbf2d6a1dabc27cf69798df1267e9221d6
James King <james.king@facepunchstudios.com>
2024-06-13 17:10:14 +0100

    Reset player balance at start of half

---
+++ Code/GameMode/Rules/Equipment/Money.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox.Events;
+
+namespace Facepunch;
+
+/// <summary>
+/// Reset player cash to the given amount when entering this state, or when they are assigned to a team.
+/// </summary>
+public sealed class ResetBalance : Component,
+	IGameEventHandler<EnterStateEvent>,
+	IGameEventHandler<TeamAssignedEvent>
+{
+	[Property]
+	public int Value { get; set; } = 800;
+
+	void IGameEventHandler<EnterStateEvent>.OnGameEvent( EnterStateEvent eventArgs )
+	{
+		foreach ( var player in GameUtils.ActivePlayers )
+		{
+			player.Inventory.SetCash( Value );
+		}
+	}
+
+	void IGameEventHandler<TeamAssignedEvent>.OnGameEvent( TeamAssignedEvent eventArgs )
+	{
+		eventArgs.Player.Inventory.SetCash( Value );
+	}
+}
