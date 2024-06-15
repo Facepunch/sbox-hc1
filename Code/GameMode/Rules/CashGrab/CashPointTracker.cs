306784cd2e3b0955f8498c49bb1b626d0ddae306
Tony Ferguson <devultj@gmail.com>
2024-06-15 13:35:47 +0100

    Track who's holding the cash bag

--- Code/GameMode/Rules/CashGrab/CashPointTracker.cs
+++ Code/GameMode/Rules/CashGrab/CashPointTracker.cs
@@ -1,13 +1,21 @@
 using Sandbox;
+using Sandbox.Events;
 
 namespace Facepunch;
 
-public sealed class CashPointTracker : Component
+public sealed class CashPointTracker : Component, IGameEventHandler<EquipmentDroppedEvent>, IGameEventHandler<EquipmentPickedUpEvent>
 {
-	public HashSet<CashPoint> All { get; set; } = new();
+	[Property] public EquipmentResource Resource { get; set; }
 
+	public HashSet<CashPoint> All { get; set; } = new();
 	public CashPoint Last { get; set; }
 	public CashPoint Current { get; set; }
+
+	/// <summary>
+	/// Who is holding the bag RIGHT now?
+	/// </summary>
+	public PlayerController Holder { get; set; }
+
 
 	protected override void OnStart()
 	{
@@ -15,5 +23,32 @@ public sealed class CashPointTracker : Component
 		{
 			All.Add( cashPoint );
 		}
+	}
+
+	/// <summary>
+	/// Called when someone drops a piece of equipment.
+	/// </summary>
+	/// <param name="eventArgs"></param>
+	void IGameEventHandler<EquipmentDroppedEvent>.OnGameEvent( EquipmentDroppedEvent eventArgs )
+	{
+		if ( eventArgs.Dropped.Resource != Resource )
+			return;
+
+		if ( eventArgs.Player == Holder )
+		{
+			Holder = null;
+		}
+	}
+
+	/// <summary>
+	/// Called when someone picks up a piece of equipment
+	/// </summary>
+	/// <param name="eventArgs"></param>
+	void IGameEventHandler<EquipmentPickedUpEvent>.OnGameEvent( EquipmentPickedUpEvent eventArgs )
+	{
+		if ( eventArgs.Dropped.Resource != Resource )
+			return;
+
+		Holder = eventArgs.Player;
 	}
 }
