85b7362ed99b8a1cff425506e863bf3f1de7ba90
Tony Ferguson <devultj@gmail.com>
2024-06-15 15:02:00 +0100

    Transition to picked up state when someone picks up the cash

--- Code/GameMode/Rules/CashGrab/ActivateCashPoint.cs
+++ Code/GameMode/Rules/CashGrab/ActivateCashPoint.cs
@@ -2,8 +2,10 @@ using Sandbox.Events;
 
 namespace Facepunch;
 
-public partial class ActivateCashPoint : Component, IGameEventHandler<EnterStateEvent>
+public partial class ActivateCashPoint : Component, IGameEventHandler<EnterStateEvent>, IGameEventHandler<EquipmentPickedUpEvent>
 {
+	[Property] public StateComponent PickedUpState { get; set; }
+
 	public void OnGameEvent( EnterStateEvent eventArgs )
 	{
 		if ( !Networking.IsHost )
@@ -11,5 +13,15 @@ public partial class ActivateCashPoint : Component, IGameEventHandler<EnterState
 
 		var cashPoint = GameMode.Instance.Get<CashPointTracker>().Current;
 		cashPoint.Activate();
+	}
+
+	void IGameEventHandler<EquipmentPickedUpEvent>.OnGameEvent( EquipmentPickedUpEvent eventArgs )
+	{
+		var tracker = GameMode.Instance.Get<CashPointTracker>();
+
+		if ( eventArgs.Dropped.Resource == tracker.Resource )
+		{
+			GameMode.Instance.StateMachine.Transition( PickedUpState );
+		}
 	}
 }
