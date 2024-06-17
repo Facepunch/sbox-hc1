8d74b8aa7fe247297bce7f07c2c2235cf1c4d1df
Tony Ferguson <devultj@gmail.com>
2024-06-17 11:17:11 +0100

    Spawn a big burst of cash when someone dies holding the bag

---
+++ Code/Weapons/Components/OnKillEquipmentComponent.cs
@@ -0,0 +1,22 @@
+using Sandbox.Events;
+
+namespace Facepunch;
+
+/// <summary>
+/// A weapon component. This can be anything that controls a weapon. Aiming, recoil, sway, shooting..
+/// </summary>
+[Icon( "track_changes" )]
+public partial class OnKillEquipmentComponent : EquipmentComponent, IGameEventHandler<KillEvent>
+{
+	[Property] public GameObject PrefabToSpawn { get; set; }
+
+	void IGameEventHandler<KillEvent>.OnGameEvent( KillEvent eventArgs )
+	{
+
+		if ( GameUtils.GetPlayerFromComponent( eventArgs.DamageInfo.Victim ) is { } player 
+			&& player == Equipment.PlayerController )
+		{
+			PrefabToSpawn.Clone( Equipment.Transform.Position );
+		}
+	}
+}
