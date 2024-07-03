1094c33adb584ad522bcf31357ba402c3f43e615
James King <james.king@facepunchstudios.com>
2024-07-03 16:08:42 +0100

    ActiveBombSiteSelector for retakes

---
+++ Code/GameMode/Rules/Defuse/ActiveBombSiteSelector.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.Events;
+
+namespace Facepunch;
+
+/// <summary>
+/// Selects one bomb site to be active at the start of this state, enabling spawn points with that
+/// site's name as a tag.
+/// </summary>
+public sealed class ActiveBombSiteSelector : Component,
+	IGameEventHandler<EnterStateEvent>
+{
+	[HostSync]
+	public Guid CurrentBombSiteId { get; set; }
+
+	public BombSite CurrentBombSite => Scene.Directory.FindComponentByGuid( CurrentBombSiteId ) as BombSite;
+
+	public void OnGameEvent( EnterStateEvent eventArgs )
+	{
+		var bombSites = Scene.Components
+			.GetAll<BombSite>( FindMode.EverythingInSelfAndDescendants )
+			.ToArray();
+
+		if ( bombSites.Length == 0 )
+		{
+			throw new Exception( "No valid bomb sites!" );
+		}
+
+		var bombSite = Random.Shared.FromArray( bombSites );
+
+		CurrentBombSiteId = bombSite.Id;
+
+		var name = bombSite.GameObject.Name.ToLower();
+		var spawns = Scene.Components.GetAll<TeamSpawnPoint>( FindMode.EverythingInSelfAndDescendants );
+
+		foreach ( var spawn in spawns )
+		{
+			spawn.Enabled = spawn.GameObject.Tags.Contains( name );
+		}
+	}
+}
