6233301106dd1b4ff6ae18c1ea548a81e623bb79
James King <james.king@facepunchstudios.com>
2024-07-03 12:35:10 +0100

    Add RespawnPlayers.ForceNew

--- Code/GameMode/Rules/Spawning/RespawnPlayers.cs
+++ Code/GameMode/Rules/Spawning/RespawnPlayers.cs
@@ -8,13 +8,19 @@ namespace Facepunch;
 public sealed class RespawnPlayers : Component,
 	IGameEventHandler<EnterStateEvent>
 {
+	/// <summary>
+	/// If true, reset alive players as if they'd died.
+	/// </summary>
+	[Property]
+	public bool ForceNew { get; set; }
+
 	void IGameEventHandler<EnterStateEvent>.OnGameEvent( EnterStateEvent eventArgs )
 	{
 		GameUtils.LogPlayers();
 
 		foreach ( var player in GameUtils.AllPlayers )
 		{
-			player.Respawn( false );
+			player.Respawn( ForceNew );
 		}
 	}
 }
