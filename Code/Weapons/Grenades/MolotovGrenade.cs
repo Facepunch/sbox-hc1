ec7a4c357a54310c4bc631435fdc13894345bb7f
kurozael <kurozael@gmail.com>
2024-07-03 22:56:54 +0100

    Explode on impact properly. Fix a NRE in Crosshair

--- Code/Weapons/Grenades/MolotovGrenade.cs
+++ Code/Weapons/Grenades/MolotovGrenade.cs
@@ -7,6 +7,14 @@ public partial class MolotovGrenade : BaseGrenade, Component.ICollisionListener
 	{
 		if ( !Networking.IsHost )
 			return;
+		
+		var isValidCollision = collision.Other.Collider is MapCollider;
+
+		if ( !isValidCollision && collision.Other.GameObject.Tags.HasAny( "world", "solid" ) )
+			isValidCollision = true;
+		
+		if ( !isValidCollision )
+			return;
 
 		Explode();
 	}
