2a8e781e7414eb0b491077a77a4b8cf9bce97820
James King <james.king@facepunchstudios.com>
2024-07-03 17:27:08 +0100

    Team assigner auto balance modes

--- Code/GameMode/Rules/TeamAssigner.cs
+++ Code/GameMode/Rules/TeamAssigner.cs
@@ -1,6 +1,30 @@
-﻿using Sandbox.Events;
+﻿using Facepunch.UI;
+using Sandbox.Events;
 
 namespace Facepunch;
+
+public enum AutoBalanceMode
+{
+	/// <summary>
+	/// Don't auto-balance.
+	/// </summary>
+	None,
+
+	/// <summary>
+	/// Pick random players to move.
+	/// </summary>
+	Random,
+
+	/// <summary>
+	/// Prefer to move players with the lowest scores.
+	/// </summary>
+	LowestScorers,
+
+	/// <summary>
+	/// Prefer to move players with the lowest scores from just the last round.
+	/// </summary>
+	LowestScorersLastRound
+}
 
 /// <summary>
 /// Split players into two balanced teams. If <see cref="AllowLateJoiners"/> is true,
@@ -25,12 +49,88 @@ public sealed class TeamAssigner : Component,
 	/// </summary>
 	[Property] public bool AllowLateJoiners { get; set; } = true;
 
+	/// <summary>
+	/// What sort of auto-balancing should we apply when entering this state.
+	/// </summary>
+	[Property] public AutoBalanceMode AutoBalanceMode { get; set; } = AutoBalanceMode.Random;
+
+	[After<SwapTeams>]
 	void IGameEventHandler<EnterStateEvent>.OnGameEvent( EnterStateEvent eventArgs )
 	{
-		foreach ( var player in GameUtils.GetPlayers(Team.Unassigned) )
+		AssignSpectatorsToTeams();
+		AutoBalance();
+	}
+
+	private void AssignSpectatorsToTeams()
+	{
+		foreach ( var player in GameUtils.GetPlayers( Team.Unassigned ) )
 		{
 			AssignTeam( player, true );
 		}
+	}
+
+	private void AutoBalance()
+	{
+		if ( AutoBalanceMode == AutoBalanceMode.None )
+		{
+			return;
+		}
+
+		var ts = GameUtils.GetPlayers( Team.Terrorist ).Count();
+		var cts = GameUtils.GetPlayers( Team.CounterTerrorist ).Count();
+
+		var curScore = GetTeamCountScore( ts, cts );
+
+		// delta: how many ts to move to ct
+
+		var minDelta = -cts;
+		var maxDelta = ts;
+
+		var bestScore = curScore;
+		var bestDelta = 0;
+
+		for ( var delta = minDelta; delta <= maxDelta; ++delta )
+		{
+			var newScore = GetTeamCountScore( ts - delta, cts + delta );
+
+			if ( newScore < bestScore )
+			{
+				bestScore = newScore;
+				bestDelta = delta;
+			}
+		}
+
+		if ( bestDelta == 0 )
+		{
+			return;
+		}
+
+		var fromTeam = bestDelta < 0 ? Team.CounterTerrorist : Team.Terrorist;
+
+		var toSwap = GameUtils.GetPlayers( fromTeam )
+			.OrderBy( GetScore )
+			.Take( Math.Abs( bestDelta ) )
+			.ToArray();
+
+		foreach ( var player in toSwap )
+		{
+			player.AssignTeam( fromTeam.GetOpponents() );
+
+			// Respawn the player's pawn since we might've changed their spawn
+			if ( player.PlayerPawn.IsValid() )
+				player.PlayerPawn.Respawn();
+		}
+	}
+
+	private float GetScore( PlayerState player )
+	{
+		return AutoBalanceMode switch
+		{
+			AutoBalanceMode.Random => Random.Shared.NextSingle(),
+			AutoBalanceMode.LowestScorers => player.Components.Get<PlayerScore>() is { } playerScore ? playerScore.Score : 0,
+			AutoBalanceMode.LowestScorersLastRound => player.Components.Get<PlayerScore>() is { } playerScore ? playerScore.ScoreHistory.LastOrDefault() : 0,
+			_ => 0
+		};
 	}
 
 	private void AssignTeam( PlayerState player, bool dispatch )
@@ -53,7 +153,7 @@ public sealed class TeamAssigner : Component,
 
 	public float GetTeamCountScore( int ts, int cts )
 	{
-		if ( ts > MaxTeamSize || cts > MaxTeamSize ) return float.PositiveInfinity;
+		if ( ts > MaxTeamSize || cts > MaxTeamSize || ts < 0 || cts < 0 ) return float.PositiveInfinity;
 		if ( ts == 0 || cts == 0 ) return 1000f;
 
 		var ratio = (float)ts / cts;
