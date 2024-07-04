89542ce6ed81b39481b5a475dca4fdabe9ab69c2
Tony Ferguson <devultj@gmail.com>
2024-07-04 12:49:38 +0100

    Add ScoreAttribute, instead of manually defining on scoreboard, you can define score properties on any component on a PlayerState, can define format, order, ShowIf, this'll be good when we add gamemode-created components to PlayerState

--- Code/GameMode/PlayerScore.cs
+++ Code/GameMode/PlayerScore.cs
@@ -3,6 +3,62 @@
 namespace Facepunch.UI;
 
 /// <summary>
+/// Plop this on something you're using <see cref="ScoreAttribute"/> for. We could codegen this attribute on components that use it to save this hassle.
+/// </summary>
+public interface IScore
+{
+	/// <summary>
+	/// Looks for a bunch of score attributes from components on a <see cref="PlayerState"/>, and returns a formatted, sorted list of values.
+	/// </summary>
+	/// <param name="playerState"></param>
+	/// <returns></returns>
+	public static IEnumerable<(object Value, ScoreAttribute Attribute)> Find( PlayerState playerState )
+	{
+		var components = playerState.Components.GetAll<IScore>( FindMode.EnabledInSelfAndDescendants );
+		var values = new List<(object Value, MemberDescription Member, ScoreAttribute Attribute)>();
+
+		foreach ( var comp in components )
+		{
+			var type = TypeLibrary.GetType( comp.GetType() );
+
+			foreach ( var member in type.Members )
+			{
+				if ( member.GetCustomAttribute<ScoreAttribute>() is not { } scoreAttribute )
+					continue;
+
+				// Support ShowIf, which looks for a method with a boolean return to see if we can display a value
+				var show = type.GetMethod( scoreAttribute.ShowIf )?.InvokeWithReturn<bool>( comp, null ) ?? true;
+				if ( !show )
+					continue;
+
+				// Support special formatting values
+				values.Add( (
+					string.Format( scoreAttribute.Format, type.GetValue( comp, member.Name ) ),
+					member, scoreAttribute
+				) );
+			}
+		}
+
+		return values.OrderBy( x => x.Member.GetCustomAttribute<OrderAttribute>()?.Value ?? 0 )
+			// We don't need to expose x.Member
+			.Select( x => (x.Value, x.Attribute) );
+	}
+}
+
+[AttributeUsage( AttributeTargets.Property, AllowMultiple = false )]
+public class ScoreAttribute : System.Attribute
+{
+	public string Name { get; set; }
+	public string Format { get; set; } = "0";
+	public string ShowIf { get; set; } = null;
+
+	public ScoreAttribute( string name )
+	{
+		Name = name;
+	}
+}
+
+/// <summary>
 /// Handles all the player score values.
 /// </summary>
 public sealed class PlayerScore : Component,
@@ -12,18 +68,22 @@ public sealed class PlayerScore : Component,
 	IGameEventHandler<BombPlantedEvent>,
 	IGameEventHandler<RoundCounterIncrementedEvent>,
 	IGameEventHandler<RoundCounterResetEvent>,
-	IGameEventHandler<ResetScoresEvent>
+	IGameEventHandler<ResetScoresEvent>,
+	IScore
 {
 	[Property] public PlayerState PlayerState { get; set; }
 
-	[HostSync, Property, ReadOnly] 
+	[HostSync, Property, ReadOnly, Score( "Kills" )] 
 	public int Kills { get; set; } = 0;
 
-	[HostSync, Property, ReadOnly] 
+	[HostSync, Property, ReadOnly, Score( "Deaths" )] 
 	public int Deaths { get; set; } = 0;
 
-	[HostSync, Property, ReadOnly] 
+	[HostSync, Property, ReadOnly, Score( "Points" ), Order( -1 )] 
 	public int Score { get; set; } = 0;
+
+	[Score( "Ratio", Format = "0.00" ), Order( 50 )]
+	public float Ratio => (float)Kills / (float)Deaths.Clamp( 1, int.MaxValue );
 
 	[HostSync]
 	public NetList<int> ScoreHistory { get; private set; } = new();
--- Code/Player/PawnSystem/PlayerState.Money.cs
+++ Code/Player/PawnSystem/PlayerState.Money.cs
@@ -1,22 +1,28 @@
+using Facepunch.UI;
 using Sandbox.Diagnostics;
 
 namespace Facepunch;
 
-public partial class PlayerState
+public partial class PlayerState : IScore
 {
 	private int _balance = 16_000;
 
 	/// <summary>
 	/// Players current cash balance
 	/// </summary>
-	[HostSync]
+	[HostSync, Order( -100 ), Score( "Balance", Format = "${0:N0}", ShowIf = nameof( ShouldShowBalance ) )]
 	public int Balance
 	{
 		get => GameMode.Instance?.UnlimitedMoney is true ? GameMode.Instance.MaxBalance : _balance;
 		set => _balance = GameMode.Instance?.UnlimitedMoney is true ? GameMode.Instance.MaxBalance : value;
 	}
 
-	public void SetCash( int amount )
+	private bool ShouldShowBalance()
+	{
+		return GameMode.Instance?.UnlimitedMoney is not true;
+	}
+
+	public void SetCash( int amount )	
 	{
 		using var _ = Rpc.FilterInclude( Connection.Host );
 		SetCashHost( amount );
