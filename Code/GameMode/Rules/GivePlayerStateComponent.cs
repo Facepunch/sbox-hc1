b0d1ea1cc0b9ee603a2b1286a145cc999ccde2aa
James King <james.king@facepunchstudios.com>
2024-07-05 11:06:26 +0100

    GivePlayerStateComponent

---
+++ Code/GameMode/Rules/GivePlayerStateComponent.cs
@@ -0,0 +1,64 @@
+﻿using Sandbox.Events;
+
+namespace Facepunch;
+
+public abstract class GivePlayerStateComponent<T> : Component,
+	IGameEventHandler<EnterStateEvent>,
+	IGameEventHandler<LeaveStateEvent>,
+	IGameEventHandler<PlayerJoinedEvent>
+	where T : Component, new()
+{
+	private void CreateForPlayer( PlayerState player )
+	{
+		if ( player.Components.Get<T>( FindMode.EverythingInChildren ) is { } existing )
+		{
+			if ( !player.IsConnected )
+			{
+				return;
+			}
+
+			existing.GameObject.Network.AssignOwnership( player.Connection );
+
+			return;
+		}
+
+		var obj = Scene.CreateObject( false );
+
+		obj.Name = typeof(T).Name;
+
+		obj.SetParent( player.GameObject, false );
+		obj.Components.Create<T>();
+		obj.Enabled = true;
+
+		if ( player.IsConnected )
+		{
+			obj.NetworkSpawn( player.Connection );
+		}
+		else
+		{
+			obj.NetworkSpawn();
+		}
+	}
+
+	void IGameEventHandler<EnterStateEvent>.OnGameEvent( EnterStateEvent eventArgs )
+	{
+		foreach ( var player in GameUtils.AllPlayers )
+		{
+			CreateForPlayer( player );
+		}
+	}
+
+	void IGameEventHandler<LeaveStateEvent>.OnGameEvent( LeaveStateEvent eventArgs )
+	{
+		foreach ( var player in GameUtils.AllPlayers )
+		{
+			player.Components.Get<T>()?.Destroy();
+		}
+	}
+
+	void IGameEventHandler<PlayerJoinedEvent>.OnGameEvent( PlayerJoinedEvent eventArgs )
+	{
+		CreateForPlayer( eventArgs.Player );
+	}
+}
+
