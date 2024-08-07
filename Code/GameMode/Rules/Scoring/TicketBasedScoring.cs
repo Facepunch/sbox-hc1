b86c58a1a84f82c4271c20c292ad5f807e51beb6
Tony Ferguson <devultj@gmail.com>
2024-08-07 10:41:36 +0100

    Add ticket draining based on occupied capture points, can configure drain frequency, drain amount per point

--- Code/GameMode/Rules/Scoring/TicketBasedScoring.cs
+++ Code/GameMode/Rules/Scoring/TicketBasedScoring.cs
@@ -3,8 +3,12 @@
 namespace Facepunch;
 
 public sealed class TicketBasedScoring : Component,
-	IGameEventHandler<KillEvent>
+	IGameEventHandler<KillEvent>,
+	IGameEventHandler<CapturePointCapturedEvent>
 {
+	[Property] public float DrainFrequency { get; set; } = 10;
+	[Property] public int DrainAmountPerPoint { get; set; } = 1;
+
 	void IGameEventHandler<KillEvent>.OnGameEvent( KillEvent eventArgs )
 	{
 		if ( !Networking.IsHost )
@@ -30,6 +34,35 @@ public sealed class TicketBasedScoring : Component,
 		GameMode.Instance.Get<TeamScoring>()?.IncrementScore( killerPlayer.Team, -1 );
 	}
 
-	// TODO: capture event from capture point
-	// TODO: tick event that looks at all capture points and drains tickets
+	public RealTimeUntil TimeUntilTick { get; set; } = 10;
+
+	protected override void OnStart()
+	{
+		TimeUntilTick = DrainFrequency;
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if ( !Networking.IsHost )
+			return;
+
+		if ( !TimeUntilTick )
+			return;
+
+		foreach ( var capturePoint in Scene.GetAllComponents<CapturePoint>() )
+		{
+			if ( capturePoint.Team != Team.Unassigned )
+			{
+				// Hurt the opponents tickets
+				GameMode.Instance.Get<TeamScoring>()?.IncrementScore( capturePoint.Team.GetOpponents(), -1 );
+			}
+		}
+
+		TimeUntilTick = DrainFrequency;
+	}
+
+	void IGameEventHandler<CapturePointCapturedEvent>.OnGameEvent( CapturePointCapturedEvent eventArgs )
+	{
+		// TODO: Scores from capturing? Maybe?
+	}
 }
