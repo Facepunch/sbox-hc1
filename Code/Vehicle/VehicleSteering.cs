2aea36536025fde18debfaf2374538ba6264654a
Alex <12881812+xezno@users.noreply.github.com>
2024-08-07 14:11:27 +0100

    Rotate wheels individually

--- Code/Vehicle/VehicleSteering.cs
+++ Code/Vehicle/VehicleSteering.cs
@@ -2,8 +2,10 @@
 
 public sealed class Steering : Component
 {
+	[Property] public List<GameObject> Wheels { get; set; }
 	[Property] public float MaxSteeringAngle { get; set; } = 20f;
 	[Property] public float SteeringSmoothness { get; set; } = 10f;
+	[Property] public Angles Offset { get; set; }
 
 	private Vehicle ParentVehicle;
 
@@ -19,7 +21,10 @@ public sealed class Steering : Component
 
 		var inputState = ParentVehicle.InputState;
 
-		var targetRotation = Rotation.FromYaw( MaxSteeringAngle * inputState.direction.y );
-		Transform.LocalRotation = Rotation.Lerp( Transform.LocalRotation, targetRotation, Time.Delta * SteeringSmoothness );
+		foreach ( var wheel in Wheels )
+		{
+			var targetRotation = Rotation.FromYaw( MaxSteeringAngle * inputState.direction.y ) * Rotation.From( Offset );
+			wheel.Transform.LocalRotation = Rotation.Lerp( wheel.Transform.LocalRotation, targetRotation, Time.Delta * SteeringSmoothness );
+		}
 	}
 }
