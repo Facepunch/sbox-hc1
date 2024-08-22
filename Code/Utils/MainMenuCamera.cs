df3e7fa029882890e7a9d305baf1e280f3860d80
Tony Ferguson <devultj@gmail.com>
2024-08-22 23:00:30 +0100

    Add camera shake to main menu, replaced ui button hover sound

--- Code/Utils/MainMenuCamera.cs
+++ Code/Utils/MainMenuCamera.cs
@@ -1,4 +1,4 @@
-using Sandbox;
+using Sandbox.Utility;
 
 namespace Facepunch;
 
@@ -11,6 +11,18 @@ public sealed class MainMenuCamera : Component
 
 	float lerpedVal;
 
+	[Property] 
+	public float positionAmplitude { get; set; } = 0.5f;
+
+	[Property]
+	public float positionFrequency { get; set; } = 10f;
+
+	[Property] 
+	public float rotationAmplitude { get; set; } = 1f;
+
+	[Property]
+	public float rotationFrequency { get; set; } = 10f;
+
 	protected override void OnUpdate()
 	{
 		var x = (float)TimeUntilAnimComplete.Fraction.Clamp( 0f, 1f );
@@ -18,6 +30,16 @@ public sealed class MainMenuCamera : Component
 		lerpedVal = lerpedVal.LerpTo( x, Time.Delta * 2f );
 		Transform.LocalPosition = Vector3.Right * -25f + ( Vector3.Right * 25f * lerpedVal );
 		Transform.LocalRotation = Rotation.From( 0, 12.5f - ( 12.5f * lerpedVal ), 0 );
+
+		float positionOffsetX = Noise.Perlin( Time.Now * positionFrequency, 0.0f ) * positionAmplitude - (positionAmplitude / 2.0f);
+		float positionOffsetY = Noise.Perlin( 0.0f, Time.Now * positionFrequency ) * positionAmplitude - (positionAmplitude / 2.0f);
+		float positionOffsetZ = Noise.Perlin( Time.Now * positionFrequency, Time.Now * positionFrequency ) * positionAmplitude - (positionAmplitude / 2.0f);
+		float rotationOffsetX = Noise.Perlin( Time.Now * rotationFrequency, 0.0f ) * rotationAmplitude - (rotationAmplitude / 2.0f);
+		float rotationOffsetY = Noise.Perlin( 0.0f, Time.Now * rotationFrequency ) * rotationAmplitude - (rotationAmplitude / 2.0f);
+		float rotationOffsetZ = Noise.Perlin( Time.Now * rotationFrequency, Time.Now * rotationFrequency ) * rotationAmplitude - (rotationAmplitude / 2.0f);
+
+		Transform.LocalPosition += new Vector3( positionOffsetX, positionOffsetY, positionOffsetZ );
+		Transform.LocalRotation *= Rotation.From( rotationOffsetX, rotationOffsetY, rotationOffsetZ );
 
 		Camera.FieldOfView = 60 - (5f * lerpedVal);
 	}
