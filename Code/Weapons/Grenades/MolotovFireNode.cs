88cd51653df41606351fb9c02473f85696530e73
kurozael <kurozael@gmail.com>
2024-10-03 05:11:01 +0100

    Fix Molotov not spawning fire nodes. When a node is extinguished, give some time for its particle emitters to die nicely. Add new fire effects stolen and tweaked from Walker.

--- Code/Weapons/Grenades/MolotovFireNode.cs
+++ Code/Weapons/Grenades/MolotovFireNode.cs
@@ -5,11 +5,45 @@ public class MolotovFireNode : Component
 {
 	public static List<MolotovFireNode> All { get; set; } = new();
 
-	public void Extinguish()
+	[Property] public float LifeTime { get; set; } = 10f;
+	
+	
+	private TimeUntil TimeUntilExtinguish { get; set; }
+	private bool IsExtinguishing { get; set; }
+
+	public async void Extinguish()
 	{
+		if ( IsExtinguishing )
+			return;
+
+		IsExtinguishing = true;
+
+		var emitters = Components.GetAll<ParticleEmitter>();
+		foreach ( var emitter in emitters )
+		{
+			emitter.Enabled = false;
+		}
+
+		await Task.DelaySeconds( 1f );
 		GameObject.Destroy();
 	}
-	
+
+	protected override void OnStart()
+	{
+		TimeUntilExtinguish = LifeTime;
+		base.OnStart();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if ( TimeUntilExtinguish && !IsExtinguishing )
+		{
+			Extinguish();
+		}
+		
+		base.OnFixedUpdate();
+	}
+
 	protected override void OnEnabled()
 	{
 		All.Add( this );
--- Code/Weapons/Grenades/MolotovGrenade.cs
+++ Code/Weapons/Grenades/MolotovGrenade.cs
@@ -19,16 +19,8 @@ public partial class MolotovGrenade : BaseGrenade, Component.ICollisionListener,
 		if ( !Networking.IsHost || IsSpreadingFire )
 			return;
 		
-		var isValidCollision = collision.Other.Collider is MapCollider;
-
-		if ( !isValidCollision && collision.Other.GameObject.Tags.HasAny( "world", "solid" ) )
-			isValidCollision = true;
-		
-		if ( !isValidCollision )
-			return;
-		
 		var dot = collision.Contact.Normal.Dot( Vector3.Up );
-
+		
 		// Did we pretty much land on flat surface?
 		if ( dot > -0.5f ) return;
 		
