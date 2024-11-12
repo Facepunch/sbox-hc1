b940fde88bb50f454727b3268a380288856833e1
Tony Ferguson <devultj@gmail.com>
2024-11-12 16:02:20 +0000

    Crosshair improvements

--- Code/Settings/GameSettingsSystem.cs
+++ Code/Settings/GameSettingsSystem.cs
@@ -37,7 +37,10 @@ public class GameSettings
 	[Title( "Length" ), Group( "Crosshair" ), Range( 2, 50, 1 )]
 	public float CrosshairLength { get; set; } = 15;
 
-	[Title( "Distance" ), Group( "Crosshair" ), Range( 2, 50, 1 )]
+	[Title( "Width" ), Group( "Crosshair" ), Range( 1, 10, 1 )]
+	public float CrosshairWidth { get; set; } = 2;
+
+	[Title( "Distance" ), Group( "Crosshair" ), Range( -5, 50, 0.1f )]
 	public float CrosshairDistance { get; set; } = 15;
 
 	[Title( "Color" ), Group( "Crosshair" )]
