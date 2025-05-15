c39b226ffbe2a16d2f6562437650ea87eb463db9
Tony Ferguson <devultj@gmail.com>
2025-05-15 23:28:49 +0100

    Add bolt behavior to M700
    https://files.facepunch.com/tony/1b1511b1/sbox-dev_96vfsR3iYe.mp4

---
+++ Code/Weapons/Components/Bolt.cs
@@ -0,0 +1,54 @@
+using Sandbox.Events;
+
+namespace Facepunch;
+
+[Title( "Bolt" ), Group( "Weapon Components" )]
+public partial class Boltable : WeaponInputAction,
+	IGameEventHandler<WeaponShotEvent>
+{
+	[Property, Group( "Effects" )] public GameObject EjectionPrefab { get; set; }
+
+	/// <summary>
+	/// Fetches the desired model renderer that we'll focus effects on like trail effects, muzzle flashes, etc.
+	/// </summary>
+	protected WeaponModel Effector
+	{
+		get
+		{
+			if ( IsProxy || !Equipment.ViewModel.IsValid() )
+				return Equipment.WorldModel;
+
+			return Equipment.ViewModel;
+		}
+	}
+
+	private bool CanBolt = false;
+
+	void IGameEventHandler<WeaponShotEvent>.OnGameEvent( WeaponShotEvent eventArgs )
+	{
+		CanBolt = true;
+	}
+
+	protected override void OnInputUp()
+	{
+		if ( !CanBolt ) return;
+
+		Equipment.ViewModel?.ModelRenderer?.Set( "b_reload_bolt", true );
+
+		// Eject casing using GameObject / prefab
+		if ( EjectionPrefab.IsValid() )
+		{
+			if ( Effector.EjectionPort.IsValid() )
+			{
+				var x = EjectionPrefab.Clone( new CloneConfig()
+				{
+					Parent = Effector.EjectionPort,
+					Transform = new(),
+					StartEnabled = true,
+					Name = $"Bullet ejection: {Equipment.GameObject}"
+				} );
+			}
+		}
+
+	}
+}
